ab391256ef42ccb97734e24a7b0de25b5156cc26
Francisco <franjonas98@hotmail.com>
2024-10-27 17:36:14 -0300

    add basic bullets structure creation and init

--- Aracnoids/src/entities/Bullets.cpp
+++ Aracnoids/src/entities/Bullets.cpp
@@ -1,2 +1,64 @@
 #include "Bullets.h"
 
+
+namespace Bullets
+{
+
+
+
+	Bullet CreateBullet(Bullet bullet, gamePlayer::Player player)
+	{	
+		//bullets
+		bullet.bulletPos.x = player.playerPos.x - player.radius;
+		bullet.bulletPos.y = player.playerPos.y - player.radius;
+		//hitbox
+		bullet.bulletHitBox.circlePos.x = bullet.bulletPos.x;
+		bullet.bulletHitBox.circlePos.y = bullet.bulletPos.y;
+		bullet.bulletHitBox.radius = 5.0f;
+		//sprite
+		bullet.bulletRec.x = bullet.bulletPos.x;
+		bullet.bulletRec.y = bullet.bulletPos.y;
+		bullet.bulletRec.width = 10.0f;
+		bullet.bulletRec.height = 5.0f;
+
+		bullet.pivot.x = player.pivot.x;
+		bullet.pivot.y = player.pivot.y;
+		bullet.rotation = player.rotation;
+		bullet.radius = 10.0f;
+		bullet.speed = 150.0f;
+		bullet.aceleration = player.aceleration;
+		bullet.isBulletAlive = false;
+
+		return Bullet();
+	}
+
+	void InitBullets(Bullets& playerBullets, Bullet bullet, gamePlayer::Player player)
+	{
+		playerBullets.maxBullets = 100;
+
+		for (int i = 0; i < playerBullets.maxBullets; i++)
+		{
+			playerBullets.magazine[i] = CreateBullet(bullet, player);
+		}
+	}
+
+	/*void UpdateBullet(Bullet& bullet, mouse::Mouse gameMouse)
+	{
+
+
+	}*/
+
+	void DrawBullet(Bullet bullet)
+	{
+		DrawCircleLines(static_cast<int> (bullet.bulletHitBox.circlePos.x), static_cast<int> (bullet.bulletHitBox.circlePos.y), bullet.bulletHitBox.radius, RED);
+		DrawRectanglePro(bullet.bulletRec, bullet.pivot, bullet.rotation, WHITE);
+	}
+
+	/*float GetMousePosRespectFromBullet(Bullet bullet, Vector2 mouse)
+	{
+		return 0.0f;
+	}*/
+
+	
+}
+
--- Aracnoids/src/entities/Bullets.h
+++ Aracnoids/src/entities/Bullets.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include "raylib.h"
+
+#include "mouse.h"
+#include "player.h"
+#include "game_settings/constants.h"
+
+namespace Bullets
+{
+
+	struct Bullet
+	{
+		Rectangle bulletRec;
+		Circle bulletHitBox;
+		Color bulletColor;
+		Vector2 bulletPos;
+		Vector2 pivot;
+		Vector2 direction;
+		Vector2 dirNormalizado;
+		float rotation;
+		float rotationSpeed;
+		float radius;
+		float speed;
+		float angle;
+		Vector2 aceleration;
+		bool isBulletAlive;
+	};
+
+	struct Bullets
+	{
+		int maxBullets;
+		Bullet magazine[100];
+
+	};
+
+	Bullet CreateBullet(Bullet bullet, gamePlayer::Player player);
+
+	//void UpdateBullet(Bullet& bullet, mouse::Mouse gameMouse);
+	void DrawBullet(Bullet bullet);
+
+	//float GetMousePosRespectFromBullet(Bullet bullet, Vector2 mouse);
+}
