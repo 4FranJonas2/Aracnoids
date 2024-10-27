f2c2e98bac19f1c2b95f07f51e449866843a1d3f
Francisco <franjonas98@hotmail.com>
2024-10-27 15:43:47 -0300

    add namespace GAME to main game loop on gamePlay file

--- Aracnoids/src/game_play/gamePlay.cpp
+++ Aracnoids/src/game_play/gamePlay.cpp
@@ -1,134 +1,137 @@
 #include "gamePlay.h"
 
-void RunGame()
+namespace Game
 {
-	SCENEMANAGMENT scene = SCENEMANAGMENT::NONE;
-	Rectangle gameArena;
-	gamePlayer:: Player player;
-	mouse::Mouse gameMouse;
-	gameMenu::Menu mainAndPauseMenu;
-	gameMenu::Menu credits;
-	gameMenu::Menu winLoseScreen;
-	gameMenu::Menu exitScreen;
-
-	Init(player, gameArena, gameMouse, scene, mainAndPauseMenu, winLoseScreen, exitScreen);
-
-	while (!WindowShouldClose())
+	void RunGame()
 	{
-		Input(player, scene, gameMouse, mainAndPauseMenu, winLoseScreen, exitScreen);
-		Update(player, scene, gameMouse);
-		Draw(player, gameArena, scene, mainAndPauseMenu, credits, winLoseScreen, exitScreen, gameMouse);
+		SCENEMANAGMENT scene = SCENEMANAGMENT::NONE;
+		Rectangle gameArena;
+		gamePlayer::Player player;
+		mouse::Mouse gameMouse;
+		gameMenu::Menu mainAndPauseMenu;
+		gameMenu::Menu credits;
+		gameMenu::Menu winLoseScreen;
+		gameMenu::Menu exitScreen;
+
+		Init(player, gameArena, gameMouse, scene, mainAndPauseMenu, winLoseScreen, exitScreen);
+
+		while (!WindowShouldClose())
+		{
+			Input(player, scene, gameMouse, mainAndPauseMenu, winLoseScreen, exitScreen);
+			Update(player, scene, gameMouse);
+			Draw(player, gameArena, scene, mainAndPauseMenu, credits, winLoseScreen, exitScreen, gameMouse);
+		}
+
+		Close();
 	}
 
-	Close();
-}
-
-void Init(gamePlayer::Player& player, Rectangle& gameArena, mouse::Mouse& gameMouse, SCENEMANAGMENT& scene, gameMenu::Menu& mainAndPauseMenu,
-	gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen)
-{
-	switch (scene)
+	void Init(gamePlayer::Player& player, Rectangle& gameArena, mouse::Mouse& gameMouse, SCENEMANAGMENT& scene, gameMenu::Menu& mainAndPauseMenu,
+		gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen)
 	{
-	case SCENEMANAGMENT::NONE:
-		//srand(time(0));
-		InitWindow(static_cast<int>(screenWidth), static_cast<int>(screenHeight), " RunGame by Francisco Jonas ");
-		player = gamePlayer::CreatePlayer(player);
-		gameArena = arena::createGameArena(gameArena);
-		gameMouse = mouse::CreateMouse(gameMouse);
-		mainAndPauseMenu = gameMenu::CreateMainMenu(mainAndPauseMenu);
-		winLoseScreen = gameMenu::CreateWinLoseScreen(winLoseScreen);
-		exitScreen = gameMenu::CreateExitScreen(exitScreen);
+		switch (scene)
+		{
+		case SCENEMANAGMENT::NONE:
+			//srand(time(0));
+			InitWindow(static_cast<int>(screenWidth), static_cast<int>(screenHeight), " RunGame by Francisco Jonas ");
+			player = gamePlayer::CreatePlayer(player);
+			gameArena = arena::createGameArena(gameArena);
+			gameMouse = mouse::CreateMouse(gameMouse);
+			mainAndPauseMenu = gameMenu::CreateMainMenu(mainAndPauseMenu);
+			winLoseScreen = gameMenu::CreateWinLoseScreen(winLoseScreen);
+			exitScreen = gameMenu::CreateExitScreen(exitScreen);
 
-		scene = SCENEMANAGMENT::MAINMENU;
-	default:
-		break;
+			scene = SCENEMANAGMENT::MAINMENU;
+		default:
+			break;
+		}
+
+	}
+	void Input(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse gameMouse, gameMenu::Menu& mainAndPauseMenu,
+		gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen)
+	{
+		switch (scene)
+		{
+		case SCENEMANAGMENT::MAINMENU:
+			gameMenu::InputMainMenu(mainAndPauseMenu, gameMouse, scene);
+			break;
+
+		case SCENEMANAGMENT::CREDITS:
+			gameMenu::InputCredits(scene);
+			break;
+
+		case SCENEMANAGMENT::GAME:
+			gamePlayer::InputPlayer(player);
+			break;
+
+		case SCENEMANAGMENT::WINLOSESCRREN:
+			gameMenu::InputWinLoseScreen(winLoseScreen, gameMouse, scene);
+			break;
+
+		case SCENEMANAGMENT::PAUSE:
+			gameMenu::InputPauseMenu(mainAndPauseMenu, gameMouse, scene);
+			break;
+
+		case SCENEMANAGMENT::EXIT:
+			gameMenu::InputExitScreen(exitScreen, gameMouse, scene);
+			break;
+		default:
+			break;
+		}
+	}
+	void Update(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse& gameMouse)
+	{
+		mouse::UpdateMousePos(gameMouse);
+
+		switch (scene)
+		{
+		case SCENEMANAGMENT::GAME:
+			gamePlayer::UpdatePlayer(player, gameMouse);
+			break;
+		case SCENEMANAGMENT::RESETGAME:
+
+			break;
+		case SCENEMANAGMENT::WINLOSESCRREN:
+			break;
+
+		default:
+			break;
+		}
+	}
+	void Draw(gamePlayer::Player& player, Rectangle& gameArena, SCENEMANAGMENT scene, gameMenu::Menu mainAndPauseMenu,
+		gameMenu::Menu credits, gameMenu::Menu winLoseScreen, gameMenu::Menu exitScreen, mouse::Mouse gameMouse)
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		switch (scene)
+		{
+		case SCENEMANAGMENT::MAINMENU:
+			gameMenu::DrawMainMenuorPause(mainAndPauseMenu, scene, gameMouse);
+			break;
+		case SCENEMANAGMENT::CREDITS:
+			gameMenu::DrawCredits(credits);
+			break;
+		case SCENEMANAGMENT::GAME:
+			gamePlayer::DrawPlayer(player);
+			arena::drawArena(gameArena);
+			break;
+		case SCENEMANAGMENT::WINLOSESCRREN:
+			gameMenu::DrawWinLoseScreen(winLoseScreen);
+			break;
+		case SCENEMANAGMENT::PAUSE:
+			gameMenu::DrawMainMenuorPause(mainAndPauseMenu, scene, gameMouse);
+			break;
+		case SCENEMANAGMENT::EXIT:
+			gameMenu::DrawExitMenu(exitScreen);
+			break;
+		default:
+			break;
+		}
+
+		EndDrawing();
 	}
 
-}
-void Input(gamePlayer::Player& player, SCENEMANAGMENT& scene,mouse ::Mouse gameMouse, gameMenu::Menu& mainAndPauseMenu,
-	gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen)
-{
-	switch (scene)
+	void Close()
 	{
-	case SCENEMANAGMENT::MAINMENU:
-		gameMenu::InputMainMenu(mainAndPauseMenu,gameMouse,scene);
-		break;
-
-	case SCENEMANAGMENT::CREDITS:
-		gameMenu::InputCredits(scene);
-		break;
-
-	case SCENEMANAGMENT::GAME:
-		gamePlayer::InputPlayer(player);
-		break;
-
-	case SCENEMANAGMENT::WINLOSESCRREN:
-		gameMenu::InputWinLoseScreen(winLoseScreen, gameMouse, scene);
-		break;
-
-	case SCENEMANAGMENT::PAUSE:
-		gameMenu::InputPauseMenu(mainAndPauseMenu, gameMouse, scene);
-		break;
-
-	case SCENEMANAGMENT::EXIT:
-		gameMenu::InputExitScreen(exitScreen, gameMouse, scene);
-		break;
-	default:
-		break;
+		CloseWindow();
 	}
-}
-void Update(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse& gameMouse)
-{
-	mouse::UpdateMousePos(gameMouse);
-
-	switch (scene)
-	{
-	case SCENEMANAGMENT::GAME:
-		gamePlayer::UpdatePlayer(player, gameMouse);
-		break;
-	case SCENEMANAGMENT::RESETGAME:
-		
-		break;
-	case SCENEMANAGMENT::WINLOSESCRREN:
-		break;
-
-	default:
-		break;
-	}
-}
-void Draw(gamePlayer::Player& player, Rectangle& gameArena, SCENEMANAGMENT scene, gameMenu::Menu mainAndPauseMenu,
-	gameMenu::Menu credits, gameMenu::Menu winLoseScreen, gameMenu::Menu exitScreen, mouse::Mouse gameMouse)
-{
-	BeginDrawing();
-	ClearBackground(BLACK);
-	switch (scene)
-	{
-	case SCENEMANAGMENT::MAINMENU:
-		gameMenu::DrawMainMenuorPause(mainAndPauseMenu,scene, gameMouse);
-		break;
-	case SCENEMANAGMENT::CREDITS:
-		gameMenu::DrawCredits(credits);
-		break;
-	case SCENEMANAGMENT::GAME:
-		gamePlayer::DrawPlayer(player);
-		arena::drawArena(gameArena);
-		break;
-	case SCENEMANAGMENT::WINLOSESCRREN:
-		gameMenu::DrawWinLoseScreen(winLoseScreen);
-		break;
-	case SCENEMANAGMENT::PAUSE:
-		gameMenu::DrawMainMenuorPause(mainAndPauseMenu, scene, gameMouse);
-		break;
-	case SCENEMANAGMENT::EXIT:
-		gameMenu::DrawExitMenu(exitScreen);
-		break;
-	default:
-		break;
-	}
-
-	EndDrawing();
-}
-
- void Close()
-{
-	CloseWindow();
 }
--- Aracnoids/src/game_play/gamePlay.h
+++ Aracnoids/src/game_play/gamePlay.h
@@ -7,16 +7,19 @@
 #include "game_settings/scene_manage.h"
 #include "game_settings/constants.h"
 
-void RunGame();
+namespace Game
+{
+	void RunGame();
 
-void Init(gamePlayer::Player& player, Rectangle& gameArena, mouse::Mouse& gameMouse, SCENEMANAGMENT& scene, gameMenu::Menu& mainAndPauseMenu,
-	gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen);
+	void Init(gamePlayer::Player& player, Rectangle& gameArena, mouse::Mouse& gameMouse, SCENEMANAGMENT& scene, gameMenu::Menu& mainAndPauseMenu,
+		gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen);
 
-void Input(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse gameMouse, gameMenu::Menu& mainAndPauseMenu,
-	 gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen);
+	void Input(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse gameMouse, gameMenu::Menu& mainAndPauseMenu,
+		 gameMenu::Menu& winLoseScreen, gameMenu::Menu& exitScreen);
 
-void Update(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse& gameMouse);
+	void Update(gamePlayer::Player& player, SCENEMANAGMENT& scene, mouse::Mouse& gameMouse);
 
-void Draw(gamePlayer::Player& player, Rectangle& gameArena, SCENEMANAGMENT scene, gameMenu::Menu mainAndPauseMenu,
-	gameMenu::Menu credits, gameMenu::Menu winLoseScreen, gameMenu::Menu exitScreen, mouse::Mouse gameMouse);
-void Close();
+	void Draw(gamePlayer::Player& player, Rectangle& gameArena, SCENEMANAGMENT scene, gameMenu::Menu mainAndPauseMenu,
+		gameMenu::Menu credits, gameMenu::Menu winLoseScreen, gameMenu::Menu exitScreen, mouse::Mouse gameMouse);
+	void Close();
+}
