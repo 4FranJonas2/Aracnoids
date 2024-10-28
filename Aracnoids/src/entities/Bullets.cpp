b8e46d54ad95bd8dd41918fa5f39583e40e745bf
Francisco <franjonas98@hotmail.com>
2024-10-27 21:52:36 -0300

    add bullet main structure

--- Aracnoids/src/entities/Bullets.cpp
+++ Aracnoids/src/entities/Bullets.cpp
@@ -1,11 +1,8 @@
 #include "Bullets.h"
 
 
-namespace Bullets
+namespace playerBullet
 {
-
-
-
 	Bullet CreateBullet(Bullet bullet, gamePlayer::Player player)
 	{	
 		//bullets
@@ -25,14 +22,18 @@ namespace Bullets
 		bullet.pivot.y = player.pivot.y;
 		bullet.rotation = player.rotation;
 		bullet.radius = 10.0f;
-		bullet.speed = 150.0f;
+		bullet.velocity = 200.0f;
+		bullet.impulse = 0.2f;
 		bullet.aceleration = player.aceleration;
+
+		bullet.maxTimeAlive = 3.0;
+		bullet.bulletTimeAlive = 0.0;
 		bullet.isBulletAlive = false;
 
 		return Bullet();
 	}
 
-	void InitBullets(Bullets& playerBullets, Bullet bullet, gamePlayer::Player player)
+	void InitBullets(Magazine& playerBullets, Bullet bullet, gamePlayer::Player player)
 	{
 		playerBullets.maxBullets = 100;
 
@@ -42,16 +43,52 @@ namespace Bullets
 		}
 	}
 
-	/*void UpdateBullet(Bullet& bullet, mouse::Mouse gameMouse)
+	void UpdateBullet(Magazine& playerBullets, mouse::Mouse gameMouse, Bullet bullet, gamePlayer::Player player)
 	{
+		//disparo bala si se da la orden
+		if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+		{
+			for (int i = 0; i < playerBullets.maxBullets; i++)
+			{
+				if (!playerBullets.magazine[i].isBulletAlive)
+				{
+					playerBullets.magazine[i] = CreateBullet(bullet,player);
+					playerBullets.magazine[i].isBulletAlive = true;
+					playerBullets.magazine[i].bulletTimeAlive = clock();
 
+					break;
+				}
+			}
+		}
+		//si no disparo reviso estado actual del cargador
+		else if (IsMouseButtonUp(MOUSE_BUTTON_RIGHT))
+		{
+			for (int i = 0; i < playerBullets.maxBullets; i++)
+			{
+				if (playerBullets.magazine[i].isBulletAlive)
+				{
+					playerBullets.magazine[i].bulletTimeAlive = clock() - playerBullets.magazine[i].bulletTimeAlive;
 
-	}*/
+					if (playerBullets.magazine[i].bulletTimeAlive = clock() == playerBullets.magazine[i].maxTimeAlive)
+					{
+						playerBullets.magazine[i].isBulletAlive = false;
+					}
+				}
+			}
+		}
+	}
 
-	void DrawBullet(Bullet bullet)
+	void DrawBullet(Magazine playerBullets)
 	{
-		DrawCircleLines(static_cast<int> (bullet.bulletHitBox.circlePos.x), static_cast<int> (bullet.bulletHitBox.circlePos.y), bullet.bulletHitBox.radius, RED);
-		DrawRectanglePro(bullet.bulletRec, bullet.pivot, bullet.rotation, WHITE);
+		for (int i = 0; i < playerBullets.maxBullets; i++)
+		{
+			if (playerBullets.magazine[i].isBulletAlive)
+			{
+				DrawCircleLines(static_cast<int> (playerBullets.magazine[i].bulletHitBox.circlePos.x),
+					static_cast<int> (playerBullets.magazine[i].bulletHitBox.circlePos.y), playerBullets.magazine[i].bulletHitBox.radius, RED);
+				DrawRectanglePro(playerBullets.magazine[i].bulletRec, playerBullets.magazine[i].pivot, playerBullets.magazine[i].rotation, WHITE);
+			}
+		}
 	}
 
 	/*float GetMousePosRespectFromBullet(Bullet bullet, Vector2 mouse)
--- Aracnoids/src/entities/Bullets.h
+++ Aracnoids/src/entities/Bullets.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include "raylib.h"
+#include "ctime"
 
 #include "mouse.h"
 #include "player.h"
 #include "game_settings/constants.h"
 
-namespace Bullets
+namespace playerBullet
 {
 
 	struct Bullet
 	{
+		clock_t bulletTimeAlive;
 		Rectangle bulletRec;
 		Circle bulletHitBox;
 		Color bulletColor;
@@ -21,17 +23,18 @@ namespace Bullets
 		float rotation;
 		float rotationSpeed;
 		float radius;
-		float speed;
+		float impulse;
+		float velocity;
 		float angle;
 		Vector2 aceleration;
+		double maxTimeAlive;
 		bool isBulletAlive;
 	};
 
-	struct Bullets
+	struct Magazine
 	{
 		int maxBullets;
 		Bullet magazine[100];
-
 	};
 
 	Bullet CreateBullet(Bullet bullet, gamePlayer::Player player);
